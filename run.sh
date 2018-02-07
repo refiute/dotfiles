cf22d17d4041d0a4bdfe49e6698e9bd51768acb9
Katsuki Chousa <refiute1016@gmail.com>
2018-02-07 21:29:13 +0900

    add symlink for tmux

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ if [ ! -d $HOME/dotfiles ]; then
 fi
 
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 
 mkdir -p $HOME/.config
 ln -s $HOME/dotfiles/vim $HOME/.config/nvim
