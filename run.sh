12e620c791f87087fdeea2dcf74817c3ae20f6c3
Katsuki Chousa <refiute1016@gmail.com>
2018-02-07 21:24:27 +0900

    fix start script

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ git config --global user.name "Katsuki Chousa"
 git config --global user.email refiute1016@gmail.com
 git config --global color.ui auto
 git config --global core.editor 'vim -c "set fenc=utf-8"'
-if [ -d $HOME/dotfiles ]; then
-	git clone "https://github.com/refiute/dotfiles"
+if [ ! -d $HOME/dotfiles ]; then
+	git clone "https://github.com/refiute/dotfiles" ~/dotfiles
 fi
 
 ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
